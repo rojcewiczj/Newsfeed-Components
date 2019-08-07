3652415dd1c269eca9bfc8d212c7e5363157939e
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-07 13:54:47 -0500

    setting up function, defining elements

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,9 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+
+function createArticle (title, date, firstP, secondP, thirdP) {
+//defining new elements
+
+
+}
